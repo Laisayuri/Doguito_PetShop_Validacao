3ce39f4735e1fc5b66e37453d8d4b3cb41419f97
Lais <lais.yasunaka@gmail.com>
2022-01-05 00:08:10 -0300

    update

--- js/validacao.js
+++ js/validacao.js
@@ -1,22 +1,29 @@
-  const dataNascimento = document.querySelector('nascimento')
+export function valida(input) {
+  const tipoDeInput = input.dataset.tipo
 
-  dataNascimento.addEventListener('blur', (evento) => {
-      validaDataNascimento(evento.target)
-  })
-  
-  function validaDataNascimento(input) {
-    const dataRecebida = new Date(input.value)
-     let mensagem = ''    
+  if(validadores[tipoDeInput]) {
+      validadores[tipoDeInput](input)
+  }
+}
 
-    if(!maiorQue18(dataRecebida)) {
-    mensagem = 'Você deve ser maior que 18 anos para se cadastrar.'
-    }
-    input.setCustomValidity(mensagem)
+const validadores = {
+  dataNascimento:input => validaDataNascimento(input)
+}
+
+function validaDataNascimento(input) {
+  const dataRecebida = new Date(input.value)
+  let mensagem = ''
+
+  if(!maiorQue18(dataRecebida)) {
+      mensagem = 'Você deve ser maior que 18 anos para se cadastrar.'
   }
 
-  function maiorQue18(data) {
-      const dataAtual = new Date()
-      const dataMais18 = new Date(data.getUTCFullYear() + 18, data.getUTCMonth(), data.getUTCDate())
+  input.setCustomValidity(mensagem)
+}
 
-      return dataMais18 <= dataAtual  
-  }
+function maiorQue18(data) {
+  const dataAtual = new Date()
+  const dataMais18 = new Date(data.getUTCFullYear() + 18, data.getUTCMonth(), data.getUTCDate())
+
+  return dataMais18 <= dataAtual
+}
